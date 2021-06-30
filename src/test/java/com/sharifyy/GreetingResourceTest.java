e4503c6eb28ad91923ac75ddbdbd9332e462a326
Mohamad <sharifyy@gmail.com>
2021-06-30 15:03:06 +0430

    editted test

--- src/test/java/com/sharifyy/GreetingResourceTest.java
+++ src/test/java/com/sharifyy/GreetingResourceTest.java
@@ -12,10 +12,10 @@ public class GreetingResourceTest {
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello-resteasy")
+          .when().get("/frameworks")
           .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+             .statusCode(200);
+//             .body(is("Hello RESTEasy"));
     }
 
 }
